527652b45d5ceed9c4ca762913db032eea4ca06b
Leroy van Engelen <leroy.vanengelen@gmail.com>
2019-06-08 12:45:55 +0200

    Exercise 3.25

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,8 +6,8 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
-
-
-
-
+  def size(t: Tree[_]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(l, r) => size(l) + size(r) + 1
+  }
 }
---
+++ exercises/src/test/scala/fpinscala/datastructures/Tree.scala
@@ -0,0 +1,28 @@
+package fpinscala.datastructures
+
+import org.scalatest.FlatSpec
+
+class TreeSpec extends FlatSpec {
+  import fpinscala.datastructures.Tree
+
+  val exampleTree1 = Branch(
+    Branch(
+      Branch(
+        Leaf(1),
+        Leaf(2)
+      ),
+      Leaf(3)
+    ),
+    Branch(
+      Leaf(4),
+      Branch(
+        Leaf(5),
+        Leaf(6)
+      )
+    )
+  )
+
+  "The size of a tree" should "be the number of leaves and branches in it" in {
+    assert(Tree.size(exampleTree1) === 11)
+  }
+}
